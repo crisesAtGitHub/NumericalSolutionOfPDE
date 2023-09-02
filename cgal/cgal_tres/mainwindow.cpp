7f1e3b217bb82e38e5e1e74f0ba4c90f071dba19
David Baltazar <david-balta@hotmail.com>
2023-09-02 16:04:41 -0600

    Tarea 3: Se agregaron los  segmentos para los envolvente convexa

--- cgal/cgal_tres/mainwindow.cpp
+++ cgal/cgal_tres/mainwindow.cpp
@@ -38,6 +38,37 @@ void MainWindow::paintEvent(QPaintEvent *event)
         q=it.operator *();
         painter.drawEllipse(q.x()-5,q.y()-5,10,10);
     }
+
+
+    // Dibujar aristas
+    painter.setPen(QPen(Qt::black)); // Escogemos el color y la anchura de la linea
+    if(!puntosEnvolventeConvexa.empty()) { // Si la lista de puntos de la envolvente convexa esta vacia no hacemos nada
+
+        // Definimos el primer punto de la lista
+        QPoint primerPunto(
+            static_cast<int>(puntosEnvolventeConvexa.first().x()),
+            static_cast<int>(puntosEnvolventeConvexa.first().y())
+        );
+
+        QPoint puntoAnterior = primerPunto;
+
+        // Recorremos punto a punto y dibujamos una linea entre el punto anterior y el punto actual
+        for(int i = 1; i < puntosEnvolventeConvexa.size(); ++i) {
+            QPoint puntoActual(
+                static_cast<int>(puntosEnvolventeConvexa.at(i).x()),
+                static_cast<int>(puntosEnvolventeConvexa.at(i).y())
+            );
+
+            painter.drawLine(puntoAnterior, puntoActual);
+
+            puntoAnterior = puntoActual;
+        }
+
+        // Dibujamos una linea entre el ultimo punto y el primero de la lista para cerrar la envolvente
+        painter.drawLine(puntoAnterior, primerPunto);
+
+    }
+
     painter.end();
     QWidget::paintEvent(event);
 }
